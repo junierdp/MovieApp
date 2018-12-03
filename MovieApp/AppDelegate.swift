b3045bde53af347bae2c38b1508cdf85fa164a9e
Junier Damian <onieldp@gmail.com>
2018-12-03 16:36:06 -0400

    Updating Fabric and addin app icons

--- MovieApp/AppDelegate.swift
+++ MovieApp/AppDelegate.swift
@@ -20,7 +20,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         FirebaseApp.configure()
-
+        Fabric.with([Crashlytics.self])
+        
         return true
     }
 
